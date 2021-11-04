e2ff54e518feef121418e5c181648731fda8371b
ajietextd <41898714+ajietextd@users.noreply.github.com>
2021-11-04 14:36:42 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   //twitter: 'transitive_bs',
-  github: 'ajietextd',
+  //github: 'ajietextd',
   //linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
